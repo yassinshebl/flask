af30587f3817965377b63095cd5a9badd4994bb0
Yassin Shebl <yassinshebl2003@gmail.com>
2024-04-20 20:48:41 +0200

    added name.html

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # create flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+#create a form class 
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # create a route decorator
 @app.route('/')
@@ -13,3 +22,8 @@ def index():
 
 def link():
     return render_template("linkk.html")
+
+#create name page
+@app.route('/name', methods = ['GET','POST'])
+def name():
+    return render_template("user.html")
